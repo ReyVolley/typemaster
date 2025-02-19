58fd010344c5fa10d7404d23c8da32011aded0c5
ReyVolley <reyyjeiser@gmail.com>
2025-02-19 13:22:22 -0400

    Update typemaster.py

--- typemaster.py
+++ typemaster.py
@@ -1,14 +1,18 @@
-print("test")
 import random
 
 words = ["bug","test","virus"]
+right = 0
+wrong = 0
+
 while words:
     random_choice = random.choice(words)
     print(random_choice)
     guess = input("Enter a word: ")
     if(guess == random_choice):
         print("correct!")
+        right += 1
     else:
         print("wrong")
+        wrong += 1
     
     words.remove(random_choice)
